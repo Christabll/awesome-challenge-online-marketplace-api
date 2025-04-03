2f6fff43f97a70d166b69ed64510a30132f49184
Christa22 <ishimwechristabellah1@gmail.com>
2025-04-03 22:40:05 +0200

    Fix test DB config with H2 + trigger CI for coverage

--- src/test/java/com/awesomity/marketplace/marketplace_api/MarketplaceApiApplicationTests.java
+++ src/test/java/com/awesomity/marketplace/marketplace_api/MarketplaceApiApplicationTests.java
@@ -2,7 +2,10 @@ package com.awesomity.marketplace.marketplace_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+
+@ActiveProfiles("test")
 @SpringBootTest
 class MarketplaceApiApplicationTests {
 
