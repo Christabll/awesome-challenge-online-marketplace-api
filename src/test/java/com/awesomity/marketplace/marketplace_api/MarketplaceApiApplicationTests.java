dd88b7319c28bdb553f14fd940ee655c8b09b4fa
Christa22 <ishimwechristabellah1@gmail.com>
2025-04-04 20:24:45 +0200

    Fix test context load by mocking JavaMailSender in TestConfig

--- src/test/java/com/awesomity/marketplace/marketplace_api/MarketplaceApiApplicationTests.java
+++ src/test/java/com/awesomity/marketplace/marketplace_api/MarketplaceApiApplicationTests.java
@@ -2,17 +2,20 @@ package com.awesomity.marketplace.marketplace_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
-@Import(TestConfig.class)
 class MarketplaceApiApplicationTests {
+
+	@MockBean
+	private JavaMailSender javaMailSender;
 
 	@Test
 	void contextLoads() {
-		System.out.println("🔧 Running with test profile...");
+		System.out.println("🐛 Running with test profile...");
 	}
-
 }
